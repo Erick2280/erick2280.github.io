90de8124ee8b4497841ed3fe3517f7042b87941d
Erick Almeida <erickingles@gmail.com>
2021-06-28 04:48:31 -0300

    Adiciona ícone para dispositivos iOS

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,8 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
     link: [
-      { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png'},
     ]
   },
 
