6f7fba60ddbee69437529e8de08a19b4b8e88f96
Erick Almeida <Erick2280@users.noreply.github.com>
2021-08-31 03:41:37 -0300

    Corrige duplicidade de declaração do charset

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,6 @@ export default {
       lang: 'pt-BR'
     },
     meta: [
-      { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
     link: [
